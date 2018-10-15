56c88af3c1531002e1e9e910353a0891fce61f87
Roberto Glez <roberto_glez@outlook.com>
2018-10-15 10:58:37 -0500

    Conexion Firebase

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public users:Observable<any>;
 
-  constructor() { }
+  constructor(db:AngularFirestore) {
+    this.users = db.collection('usuarios').valueChanges();
+    this.users.subscribe((u)=>{
+      console.log("Usuario ->",u);
+    });
+  }
 
   ngOnInit() {
   }
